b7620d35fae0d0ae7369dde88cf0c11bbe4c7f79
Daniel Khromov <danielkhromov@gmail.com>
2020-06-27 14:27:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import copy
 import json
 import logging
@@ -7,9 +6,9 @@ from threading import Thread
 import psycopg2
 from flask import Flask, request, render_template
 
-
 sessionStorage = {}
 app = Flask('')
+
 
 @app.route('/post', methods=['POST'])
 def main():
@@ -27,14 +26,27 @@ def main():
     return json.dumps(response)
 
 
-
 def handle_dialog(req, res):
     if req['session']['new']:
         res['response']['text'] = 'Добро пожаловать в словесную игру ПАС. Давай знакомиться! Назови свое имя.'
 
+def config(user_id):
+    sessionStorage[user_id] = {
+        "nick": None,
+        'mode': '',
+        'word_id': 0,
+    }
 
 def station_dialog(req, res):
-    pass
+    user_id = req['session']['user_id']
+    if res['response']['end_session'] is True:
+        write_in_base(user_id)
+    if req['session']['new']:
+        config(user_id)
+        res['response']['test'] = 'Привет! Добро пожаловать в ПАС! Скажи имя:'
+        sessionStorage[user_id]['nick'] = 
+
+
 
 
 def run():
