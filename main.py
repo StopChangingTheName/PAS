ab11021bb21cea9091019f6a7c6e8b6d11157ca9
Daniel Khromov <danielkhromov@gmail.com>
2020-06-27 13:40:35 +0300

    write in base

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import json
+import json, psycorg2
 
+def write_in_base(user_id):
+    con = psycopg2.connect(user="indmfojfvoiiem",
+                           password="facdfb9fe6e90a07401ae02ef4e2297fa93c2bf6d205648e5d7e062c0c8da8bb",
+                           host="ec2-54-247-78-30.eu-west-1.compute.amazonaws.com",
+                           port="5432",
+                           database="dd2pdbo5s56kui")
 def main(event, context):
     res = {
         'session': event['session'],
@@ -16,6 +22,7 @@ def main(event, context):
     return json.dumps(res)
 
 def dialog(req,res):
+
     return
 
 def station_dialog(req, res):
