a05ac7141cfc5128e8914bc4e25226d2bb3a958a
Daniel Khromov <danielkhromov@gmail.com>
2020-06-27 14:05:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
-import json, psycorg2
 
-def write_in_base(user_id):
-    con = psycopg2.connect(user="indmfojfvoiiem",
-                           password="facdfb9fe6e90a07401ae02ef4e2297fa93c2bf6d205648e5d7e062c0c8da8bb",
-                           host="ec2-54-247-78-30.eu-west-1.compute.amazonaws.com",
-                           port="5432",
-                           database="dd2pdbo5s56kui")
-def main(event, context):
-    res = {
-        'session': event['session'],
-        'version': event['version'],
+import copy
+import json
+import logging
+import random
+from threading import Thread
+import psycopg2
+from flask import Flask, request, render_template
+
+
+sessionStorage = {}
+app = Flask('')
+
+@app.route('/post', methods=['POST'])
+def main():
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
         'response': {
             'end_session': False
         },
     }
-    if 'screen' in event['meta']['interfaces']:
-        dialog(event,res)
+    if 'screen' in request.json['meta']['interfaces']:
+        handle_dialog(request.json, response)
     else:
-        station_dialog(event, res)
-    
-    return json.dumps(res)
+        station_dialog(request.json, response)
+    return json.dumps(response)
 
-def dialog(req,res):
 
-    return
+
+def handle_dialog(req, res):
+    if req['session']['new']:
+        res['response']['text'] = 'Добро пожаловать в словесную игру ПАС. Давай знакомиться! Назови свое имя.'
+
 
 def station_dialog(req, res):
-    return
-    
+    pass
+
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
+
+
+if __name__ == '__main__':
+    keep_alive()
+    # app.run()
